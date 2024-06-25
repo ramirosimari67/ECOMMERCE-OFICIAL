fc7c95d68d83f9fbe31740bba30b21645609c82b
Valentina-Negrete <valen.negrete@gmail.com>
2024-06-24 21:40:43 -0300

    apiMetricas

--- static/js/apiMetricas.js
+++ static/js/apiMetricas.js
@@ -5,7 +5,12 @@
         'Content-Type': 'application/json'
         }
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+        })
     .then(data => console.log('Métricas:', data))
     .catch(error => console.error('Error:', error));
 
